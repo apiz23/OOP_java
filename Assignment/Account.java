d25535f70a0de1b0ba594f0cb620c9dea58a9c59
Sh0_ <100256725+apiz23@users.noreply.github.com>
2022-12-14 14:49:55 +0800

    Delete Account.java

--- Assignment/Account.java
+++
@@ -1,7 +0,0 @@
-package Assignment;
-
-public class Account {
-    String accountNumber = "7353294386";
-    double accountAmount = 5000;
-    double checkBalance(double amountWithdraw){return accountAmount-amountWithdraw;}
-}
