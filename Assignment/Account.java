a0daa647aac7724c41da2826c2261b2b6274ddd9
Sh0_ <100256725+apiz23@users.noreply.github.com>
2022-12-14 14:28:22 +0800

    OOP_assignment
    Code for the assignment OOP

---
+++ Assignment/Account.java
@@ -0,0 +1,7 @@
+package Assignment;
+
+public class Account {
+    String accountNumber = "7353294386";
+    double accountAmount = 5000;
+    double checkBalance(double amountWithdraw){return accountAmount-amountWithdraw;}
+}
---
+++ Assignment/Customer.java
@@ -0,0 +1,5 @@
+package Assignment;
+
+public class Customer {
+    String name = "Muhammad Hafizuddin";
+}
